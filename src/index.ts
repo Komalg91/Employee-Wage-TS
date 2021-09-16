727d82eade62c6386f22edce37e9b8577d6bcddb
Komal Ghanwat <kggk9050@gmail.com>
2021-09-17 00:30:57 +0530

    UC5_maximum working hours

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,7 @@ let emp_monthly_wage: number = 0;
 let emp_daily: number;
 let emp_daily_hour: number;
 const month_days: number = 20;
+const max_work_hours: number = 160;
 
 //UC3 daily hours
 function switch_func(random_time: number)
@@ -60,6 +61,28 @@ function monthly_wage_func(){
     console.log("Monthly wage: ", emp_monthly_wage )
 }
 
+//UC5 maximum working hours
+function max_work_hours_func(){
+    let emp_month_days: number = 0;
+    let emp_working_hours: number = 0;
+    let emp_present_days: number = 0;
+    while(emp_month_days < month_days && emp_working_hours < max_work_hours){
+        let random_time: number = Math.floor(Math.random()*10)%3;
+        emp_daily_hour = switch_func(random_time);
+        let emp_daily_wage: number = daily_wage_func(emp_daily_hour);
+        emp_month_days++;
+        emp_working_hours = emp_working_hours + emp_daily_hour;
+        emp_monthly_wage = emp_monthly_wage + emp_daily_wage;
+        console.log("Day:",emp_month_days, "  Hours:", emp_daily_hour, "  Wage:",emp_daily_wage);
+
+        if(emp_daily_hour!=0){
+            emp_present_days++;
+        }
+    }
+    console.log("Days present:", emp_present_days);
+    console.log("Max hours:", emp_working_hours, " Monthly wage:", emp_monthly_wage);
+}
+
 
 let emp_atd_check: number = Math.floor(Math.random()*10)%2;
 console.log(attendance_check(emp_atd_check));
@@ -69,3 +92,4 @@ let daily_wages: number = daily_wage_func(random_time);
 console.log("Wage: " +daily_wages);
 
 monthly_wage_func();
+max_work_hours_func();
