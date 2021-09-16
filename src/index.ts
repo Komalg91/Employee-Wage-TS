a5a06f3c9c528d6477001c64bab7be2f4d539e74
Komal Ghanwat <kggk9050@gmail.com>
2021-09-16 23:38:55 +0530

    UC1_check_attendance

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
 console.log("*************** Welcome to Employee Wage Computation******************");
+
+//UC1 check employee presence
+const is_absent: number = 0;
+function attendance_check(empCheck: number): number { 
+    if(empCheck == is_absent){
+        console.log("Employee is absent");
+        empCheck = 0;
+    }
+    else{
+        console.log("Employee is present");
+        empCheck = 1;
+    }
+    return empCheck;
+}
